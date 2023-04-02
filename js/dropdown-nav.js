256d477c4ae923fe6910ce628f47bcf280e9c16c
Olena Vdovych <olena.vdovych2@gmail.com>
2023-04-02 23:48:21 +0300

    Added dropdown

---
+++ js/dropdown-nav.js
@@ -0,0 +1,6 @@
+const dropdownToggle = document.querySelector('.dropdown');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+
+dropdownToggle.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('active');
+});
